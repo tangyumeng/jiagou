a21acb1ad6a674e49a9eddeafa215c52b99f456d
tangyumeng <tangyumeng@modelbest.cn>
2025-10-10 12:16:55 +0800

    Initial Commit

---
+++ jiagou/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  jiagou
+//
+//  Created by tangyumeng on 2025/10/10.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ jiagouTests/jiagouTests.swift
@@ -0,0 +1,17 @@
+//
+//  jiagouTests.swift
+//  jiagouTests
+//
+//  Created by tangyumeng on 2025/10/10.
+//
+
+import Testing
+@testable import jiagou
+
+struct jiagouTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
